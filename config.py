72a735e14922dd05a6f08fb739648e5650e5d823
imransimsek <imrnsimsek.1616@gmail.com>
2025-04-26 16:58:17 +0300

    fixed somethings

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Binance API ayarları
-BINANCE_API_KEY = "qXcKqSvRoExKay13MPcQedAQKd1NiqJYKVSeOQdoIDztmq9MCO5FxiWJPRJ3pbCa"
-BINANCE_API_SECRET = "TnUJcH5jTvXcgZy1AG2l3RIvPerp1yrScdfcAkKVH3f8lxpXlQNIhoKhFsuOF8iw"
+BINANCE_API_KEY = os.environ.get("BINANCE_API_KEY", "")
+BINANCE_API_SECRET = os.environ.get("BINANCE_API_SECRET", "")
 
 # Telegram ayarları
-TELEGRAM_BOT_TOKEN = "7288825391:AAFzbPiVIswOpzZoWsuaPE8cFETUx2N10YU"
-TELEGRAM_CHAT_ID = "926499549"
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
 
 # Semboller ve zaman dilimleri
-SYMBOLS = ["BTCUSDT", "ETHUSDT","SOLUSDT","AVAXUSDT"]
-INTERVALS = ["5m", "15m"]
+SYMBOLS = os.environ.get("SYMBOLS", "BTCUSDT,ETHUSDT,SOLUSDT,AVAXUSDT").split(",")
+INTERVALS = os.environ.get("INTERVALS", "5m,15m").split(",")
 
 # İndikatör parametreleri
-FISHER_LENGTH = 10
-EMA_LENGTH = 5
-RANGE_OFFSET = 1.0
+FISHER_LENGTH = int(os.environ.get("FISHER_LENGTH", "21"))
+EMA_LENGTH = int(os.environ.get("EMA_LENGTH", "89"))
+RANGE_OFFSET = float(os.environ.get("RANGE_OFFSET", "2.5"))
 
 # Debug modu
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
